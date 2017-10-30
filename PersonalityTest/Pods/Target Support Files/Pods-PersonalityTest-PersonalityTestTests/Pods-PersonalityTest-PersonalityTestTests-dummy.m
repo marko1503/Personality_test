12b36f64f30fb078056ba515e5a737e42dc6cfc0
Maksym Prokopchuk <rowwingman@gmail.com>
2017-10-30 02:13:31 +0200

    Setup cocoapods

---
+++ PersonalityTest/Pods/Target Support Files/Pods-PersonalityTest-PersonalityTestTests/Pods-PersonalityTest-PersonalityTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PersonalityTest_PersonalityTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_PersonalityTest_PersonalityTestTests
+@end
